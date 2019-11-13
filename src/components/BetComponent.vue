833679f9c73dd67245ac9f6645a0550add70b61b
Yannick Spoerl <spoerlyannick@gmail.com>
2019-11-13 15:21:18 +0100

    Add live-time

--- src/components/BetComponent.vue
+++ src/components/BetComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-      BetComponent
+      <h1 class="display-4" font-color="primary">{{liveTime}}</h1>
   </v-container>
 </template>
 
@@ -9,7 +9,28 @@ export default {
   name: 'BetComponent',
 
   data: () => ({
-  
+    liveTime: ''
   }),
-};
+  methods: {
+
+  },
+  created(){
+    let self = this
+    setInterval(function(){
+      let now = new Date()
+      let minutes = now.getMinutes()
+      let seconds = now.getSeconds()
+      let timeString = now.getHours() + ':'
+      if(minutes < 10){
+        timeString += '0'
+      }
+      timeString += minutes + ':'
+      if (seconds < 10) {
+        timeString += '0'
+      }
+      timeString += seconds
+      self.liveTime = timeString
+    })
+  }
+}
 </script>
